079653fbe6656db90d2d9c2693eca7699badf364
Arminonly <amiaby2016@gmail.com>
2024-04-29 22:32:38 +0300

    установлен BrowserRouter

--- src/pages/registry/RegistryPage.jsx
+++ src/pages/registry/RegistryPage.jsx
@@ -1,168 +1,47 @@
 import { Container } from 'reactstrap';
-import { Button, Form, Input, Select, InputNumber } from 'antd';
-const { Option } = Select;
-const formItemLayout = {
-  labelCol: {
-    xs: {
-      span: 24
-    },
-    sm: {
-      span: 8
-    }
-  },
-  wrapperCol: {
-    xs: {
-      span: 24
-    },
-    sm: {
-      span: 16
-    }
-  }
-};
+// import { Button, Form, Input, Select, InputNumber, Typography } from 'antd';
+// const { Option } = Select;
 
-const tailFormItemLayout = {
-  wrapperCol: {
-    xs: {
-      span: 24,
-      offset: 0
-    },
-    sm: {
-      span: 16,
-      offset: 8
-    }
-  }
-};
+// const formItemLayout = {
+//   labelCol: {
+//     xs: {
+//       span: 24
+//     },
+//     sm: {
+//       span: 8
+//     }
+//   },
+//   wrapperCol: {
+//     xs: {
+//       span: 24
+//     },
+//     sm: {
+//       span: 16
+//     }
+//   }
+// };
+
+// const tailFormItemLayout = {
+//   wrapperCol: {
+//     xs: {
+//       span: 24,
+//       offset: 0
+//     },
+//     sm: {
+//       span: 16,
+//       offset: 8
+//     }
+//   }
+// };
 
 export const RegistryPage = () => {
-  const [form] = Form.useForm();
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
-  };
 
-  const onChange = (value) => {
-    console.log('changed', value);
-  };
 
   return (
     <>
       <Container className="bg-light border" fluid>
         <h3>Страница регистрации</h3>
-        <Form
-          {...formItemLayout}
-          form={form}
-          name="register"
-          onFinish={onFinish}
-          initialValues={{
-            residence: ['zhejiang', 'hangzhou', 'xihu'],
-            prefix: '86'
-          }}
-          style={{
-            maxWidth: 600
-          }}
-          scrollToFirstError
-        >
-          <Form.Item
-            name="nickname"
-            label="Nickname"
-            tooltip="What do you want others to call you?"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your nickname!',
-                whitespace: true
-              }
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!'
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!'
-              }
-            ]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="password"
-            label="Password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!'
-              }
-            ]}
-            hasFeedback
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item
-            name="confirm"
-            label="Confirm Password"
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Please confirm your password!'
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(
-                    new Error('The new password that you entered do not match!')
-                  );
-                }
-              })
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item
-            name="gender"
-            label="Gender"
-            rules={[
-              {
-                required: true,
-                message: 'Please select gender!'
-              }
-            ]}
-          >
-            <Select placeholder="select your gender">
-              <Option value="male">Male</Option>
-              <Option value="female">Female</Option>
-              <Option value="other">Other</Option>
-            </Select>
-          </Form.Item>
-          <Form.Item  name="number"
-            label="age">
-            <InputNumber
-         
-              min={1}
-              max={10}
-              defaultValue={3}
-              onChange={onChange}
-            />
-          </Form.Item>
-        </Form>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Register
-          </Button>
-        </Form.Item>
+    
       </Container>
     </>
   );
